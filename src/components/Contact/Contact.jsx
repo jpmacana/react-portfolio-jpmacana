7faa49a5ef1bb4519c44b24d43d70d5f87793e37
Juan Pablo Senatra <82981434+jpmacana@users.noreply.github.com>
2022-09-21 18:22:03 -0300

    Update Contact.jsx

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -35,7 +35,7 @@ const Contact = () => {
 						<h4>Email</h4>
 						<h5>jpsenatra@gmail.com</h5>
 						<a href="mailto:jpsenatra@gmail.com" target="_blank">
-							Enviar mensaje
+							Send Message
 						</a>
 					</article>
 					<article className="article contact__option">
@@ -49,7 +49,7 @@ const Contact = () => {
 							href="https://api.whatsapp.com/send?phone=5492615711988"
 							target="_blank"
 						>
-							Enviar mensaje
+							Send Message
 						</a>
 					</article>
 					<article className="article contact__option">
@@ -60,7 +60,7 @@ const Contact = () => {
 						<h4>Messenger</h4>
 						<h5>Juan Pablo Senatra</h5>
 						<a href="https://m.me/jpmacana" target="_blank">
-							Enviar mensaje
+							Send Message
 						</a>
 					</article>
 					<article className="article contact__option">
@@ -68,7 +68,7 @@ const Contact = () => {
 						<h4>Twitter</h4>
 						<h5>jpmacana</h5>
 						<a href="https://twitter.com/jpsenatra" target="_blank">
-							Enviar mensaje
+							Send Message
 						</a>
 					</article>
 				</div>
@@ -77,21 +77,21 @@ const Contact = () => {
 						type="text"
 						name="name"
 						id="name"
-						placeholder="Ingrese su Nombre completo"
+						placeholder="Enter your fullname"
 						required
 					/>
 					<input
 						type="email"
 						name="email"
 						id="email"
-						placeholder="Ingrese su correo"
+						placeholder="Enter your email"
 						required
 					/>
 					<textarea
 						name="message"
 						id="message"
 						rows="7"
-						placeholder="Ingrese su consulta"
+						placeholder="Enter your query"
 						required
 					/>
 					<button type="submit" className="btn btn-primary">
